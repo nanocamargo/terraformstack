5a661001ef0cb08fa38f07c00d60e4c09f87a802
davidcamargoOCI <david.camargo.velandia@oracle.com>
2025-06-11 15:32:16 -0500

    id tenant

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "vcn_hub"{
+tenancy_id = "ocid1.tenancy.oc1..aaaaaaaahrtcdvcu6zxtj43cbavvaglgdrziddsyw5sep2xmlsbjebaegx7q"
 source = "github.com/oracle-terraform-modules/terraform-oci-vcn"
 compartment_id = var.compartment_id
   label_prefix   = var.label_prefix
