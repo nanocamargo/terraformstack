b2c78f0f8af86e386d0eb3d66bf51b3fbb0d165f
davidcamargoOCI <david.camargo.velandia@oracle.com>
2025-06-12 17:53:25 -0500

    ultimo

--- main.tf
+++ main.tf
@@ -6,3 +6,21 @@ provider "oci" {
   region           = var.region
 }
 
+module "vcn_pruebas" {
+  source         = "./modules/vcn"
+  cidr_block     = "10.0.0.0/16"
+  compartment_id = var.compartment_id
+  display_name   = "VCN-Pruebas"
+  dns_label      = "vcntest"
+}
+
+module "database" {
+  source             = "./modules/database"
+  compartment_id     = var.compartment_id
+  db_name            = "PRUEBADB"
+  admin_password     = var.admin_password
+  cpu_core_count     = 1
+  data_storage_size  = 1
+  display_name       = "Autonomous-DB-Prueba"
+  db_workload        = "OLTP"
+}
