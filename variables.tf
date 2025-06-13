f2558adf8ea371c33eb7613d65dbfaa07b7fe72a
davidcamargoOCI <david.camargo.velandia@oracle.com>
2025-06-13 09:53:13 -0500

    version

--- variables.tf
+++ variables.tf
@@ -2,43 +2,36 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 # provider identity parameters
-#variable "fingerprint" {
- # description = "fingerprint of oci api private key"
-  #type        = string
+variable "fingerprint" {
+  description = "fingerprint of oci api private key"
+  type        = string
   # no default value, asking user to explicitly set this variable's value. see codingconventions.adoc
-#}
-
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "region" {}
-variable "private_key" {
-  sensitive = true
 }
 
-#variable "private_key" {
- # type        = string
-  #description = "Ruta local al archivo PEM"
-#}
 
-#variable "region" {
- # description = "the oci region where resources will be created"
-  #type        = string
+variable "private_key" {
+  type        = string
+  description = "Ruta local al archivo PEM"
+}
+
+variable "region" {
+  description = "the oci region where resources will be created"
+  type        = string
   # no default value, asking user to explicitly set this variable's value. see codingconventions.adoc
   # List of regions: https://docs.cloud.oracle.com/iaas/Content/General/Concepts/regions.htm#ServiceAvailabilityAcrossRegions
-#}
+}
 
-#variable "tenancy_ocid" {
- # description = "tenancy id where to create the sources"
-  #type        = string
+variable "tenancy_ocid" {
+  description = "tenancy id where to create the sources"
+  type        = string
   # no default value, asking user to explicitly set this variable's value. see codingconventions.adoc
-#}
+}
 
-#variable "user_ocid" {
- # description = "id of user that terraform will use to create the resources"
-  #type        = string
+variable "user_ocid" {
+  description = "id of user that terraform will use to create the resources"
+  type        = string
   # no default value, asking user to explicitly set this variable's value. see codingconventions.adoc
-#}
+}
 
 # general oci parameters
 
