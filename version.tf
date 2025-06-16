e019c6727f9f6896cd3abdc681915837a0ae8fae
davidcamargoOCI <david.camargo.velandia@oracle.com>
2025-06-16 15:42:22 -0500

    provider

--- version.tf
+++ version.tf
@@ -8,3 +8,10 @@ terraform {
   required_version = ">= 1.8.4"
 }
 
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key = var.private_key
+  region           = var.region
+}
