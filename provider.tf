8e18f09719166de64d183d3178bb253f92a142c2
davidcamargoOCI <david.camargo.velandia@oracle.com>
2025-08-11 15:50:58 -0500

    cambio key

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     oci = {
-      source  = "oracle/oci"
+      source  = "hashicorp/oci"
       version = ">=4.67.3"
     }
   }
@@ -12,7 +12,7 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key      = var.private_key 
+ #private_key      = var.private_key 
   region           = var.region
   private_key_path = var.private_key_path
 }
